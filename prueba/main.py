03d2532bda77012187aa5d190b187fb0a351146a
victor11s <60446086+victorponce518755@users.noreply.github.com>
2023-09-07 22:48:20 -0600

    Antes de subirlo

---
+++ prueba/main.py
@@ -0,0 +1,41 @@
+
+from flask import Flask, Response
+from flask_mysqldb import MySQL
+import xml.etree.ElementTree as ET
+
+app = Flask(__name__)
+
+# MySQL Configuration
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'srv_user'
+app.config['MYSQL_PASSWORD'] = '4335'
+app.config['MYSQL_DB'] = 'Servicios'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+
+def generate_xml_data():
+    cursor = mysql.connection.cursor()
+
+
+#en el cursor execute manda a llamar al store procedure llamado dameIngredientes, que recibe un  id de entrada que viene del link
+    cursor.execute("CALL dameIngredientes(1)")
+    info_Receta = cursor.fetchall()
+    
+
+
+    root = ET.Element('raiz')
+    for row in info_Receta:
+        ET.SubElement
+
+    tree = ET.ElementTree(root)
+    return tree
+
+@app.route('/')
+def display_xml():
+    xml_tree = generate_xml_data()
+    xml_string = ET.tostring(xml_tree.getroot(), encoding='utf-8')
+    return Response(xml_string, content_type='text/xml')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ prueba/receta.sql
@@ -0,0 +1,46 @@
+
+CREATE TABLE Ingredientes (
+	idIngrediente INT NOT NULL,
+	nombre_ingrediente VARCHAR(50) NOT NULL,
+	PRIMARY KEY (idIngrediente)
+);
+
+
+CREATE TABLE RecetaIngrediente (
+	id_receta INT NOT NULL,
+	id_ingrediente INT NOT NULL,
+	cantidad INT NOT NULL,
+	PRIMARY KEY (id_receta, id_ingrediente),
+	FOREIGN KEY (id_ingrediente) REFERENCES Ingredientes(idIngrediente),
+	FOREIGN KEY (id_receta) REFERENCES Receta(idReceta)
+);
+
+
+CREATE TABLE Receta (
+	idReceta INT NOT NULL,
+	nombre_receta VARCHAR(50) NOT NULL,
+	PRIMARY KEY (idReceta)
+);
+
+
+/*Recetas*/
+
+INSERT INTO Receta (idReceta, nombre_receta) VALUES (1, 'Tacos');
+INSERT INTO Receta (idReceta, nombre_receta) VALUES (2, 'Torta');
+INSERT INTO Receta (idReceta, nombre_receta) VALUES (3, 'Tostadas');
+
+/*Ingredientes*/
+INSERT INTO Ingredientes (idIngrediente, nombre_ingrediente) VALUES (1, 'Tortilla');
+INSERT INTO Ingredientes (idIngrediente, nombre_ingrediente) VALUES (2, 'Carne');
+INSERT INTO Ingredientes (idIngrediente, nombre_ingrediente) VALUES (3, 'Lechuga');
+INSERT INTO Ingredientes (idIngrediente, nombre_ingrediente) VALUES (4, 'Pan');
+INSERT INTO Ingredientes (idIngrediente, nombre_ingrediente) VALUES (5, 'Milanesa');
+
+
+/*Ingredientes de cada receta*/
+INSERT INTO RecetaIngrediente (id_receta, id_ingrediente, cantidad) VALUES (1, 1, 2);
+INSERT INTO RecetaIngrediente (id_receta, id_ingrediente, cantidad) VALUES (2, 5, 3);
+INSERT INTO RecetaIngrediente (id_receta, id_ingrediente, cantidad) VALUES (3, 3, 1);
+
+
+
