4d65c19a236a4b5f7c7d39dc66853818a90bbe40
victor11s <60446086+victorponce518755@users.noreply.github.com>
2023-10-18 02:26:34 -0600

    ajustes

--- pruebaF/app.py
+++ pruebaF/app.py
@@ -15,32 +15,39 @@ def serve_xsl(filename):
     return send_from_directory(app.config['XSLT_FOLDER'], filename)
 
 
+def transform_xml_to_html(xml_content):
+    xslt_file= os.path.join('xsl', 'TR.xsl')
+    xslt = etree.parse(xslt_file)
+    transform = etree.XSLT(xslt)
 
+    xml_doc= etree.fromstring(xml_content)
+    print("soy el xml doc", xml_doc)
 
-def transform_xml_to_html(xml_content, xsl_content):
-    xml_doc = etree.fromstring(xml_content)
-    xslt_doc = etree.fromstring(xsl_content)
-    transform = etree.XSLT(xslt_doc)
-    result_tree = transform(xml_doc)
-    html_content = str(result_tree)
-    return html_content
+    html_content = transform(xml_doc)
+    print("soy el html content", html_content)
+
+    html_str = str(html_content)
+    print("soy el html str", html_str)
+
+    return html_str
+
 
 @app.route('/receta/<int:id>')
 def display_xml_receta(id):
-    # Obtener XML del backend
+    #obtener xml del backend
     backend_url = 'http://35.224.154.129:5000/receta/{}'.format(id)
     response = requests.get(backend_url)
+    
 
     if response.status_code == 200:
+        #transformar xml a html
         xml_content = response.text
 
-        # Leer el contenido del archivo XSL
-        with open('xsl/TR.xsl', 'r') as xsl_file:
-            xsl_content = xsl_file.read()
-
-        # Transformar XML a HTML
-        html_content = transform_xml_to_html(xml_content, xsl_content)
-
+        #print(xml_content)
+        html_content = transform_xml_to_html(xml_content)
+        #print("--------------------------------------")
+        #print(html_content)
+        
         return Response(html_content, content_type='text/html')
     else:
         return "Error al obtener la receta"
