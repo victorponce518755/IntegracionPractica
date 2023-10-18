605f88ab1e8b88b0a22be23359529988102b49a5
victor11s <60446086+victorponce518755@users.noreply.github.com>
2023-10-18 01:51:51 -0600

    ajustes

--- pruebaF/app.py
+++ pruebaF/app.py
@@ -32,7 +32,7 @@ def transform_xml_to_html(xml_content):
 @app.route('/receta/<int:id>')
 def display_xml_receta(id):
     #obtener xml del backend
-    backend_url = '35.224.154.129:5000/receta/{}'.format(id)
+    backend_url = 'http://35.224.154.129:5000/receta/{}'.format(id)
     response = requests.get(backend_url)
 
     if response.status_code == 200:
